4f54b1e28ac33f34d9fcba3a9b7ed05f0b89fef8
Your Name <dacthang163@gmail.com>
2023-03-28 22:15:45 +0700

    Thêm phần Admin

---
+++ BTL/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL.Areas.Admin.Controllers
+{
+	public class HomeAdminController : Controller
+	{
+		[Area("Admin")]
+		[Route("Admin/HomeAdmin")]
+		[Route("HomeAdmin")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
